0d246bc55c3f8de1b3e392a03af987512917ae99
James Butler <james.butler@sandfox.co.uk>
2016-02-04 17:30:47 +0000

    rewrite internals to actually work improve readme tons of docs etc v2 basically didn't work!

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-- Creates and returns an `http` server like thing.
-- Attaches the request listener for you (attaching listeners after instantion leads to duplicate requests and bad thing)
-- Does not "start" the server
+# Silly Server Factory 
+
+Handles (some of) the pain of creating a non-ssl proxy-protocol spdy/http2 understanding server
+
+Read the `index.js` for docs/comments etc.
+
+__Basics__
+- Creates and returns an `http` server like thing
+- Attaches the request listener for you (attaching listeners after instantion leads to duplicate requests and bad things and is totally not supported and who-knows-what unexpected behaviour may happen)
+- Does not "start"/"listen" the server
+
+## Usage
+```
+var factory = require('bizzby-silly-server-factory')
+
+// This could be an express app
+var myRequestHandler = function(req, res){
+
+    if(req.isSpdy) {
+        console.log('SPDY/HTTP2 request, spdyVersion %s, stream %s', req.spdyVersion, req.spdyStream.id)
+    }
+    
+    console.log('remote IP = ' + req.connection.remoteAddress + ':' + req.connection.remotePort)
+    // these will be undefined if the connection does send the proxy protocol headers
+    console.log('proxy IP = ' + req.connection.proxyAddress + ':' + req.connection.proxyPort)
+    console.log('client IP = ' + req.connection.clientAddress + ':' + req.connection.clientPort)
+
+    res.writeHead(200)
+    res.end('ok')
+}
+
+// Set this otherwise they default to false :-)
+var someOpts = {
+  proxyprotocol: true,
+  spdy: true
+}
+
+var myServer = factory(someOpts, myRequestHandler).listen(9100)
+```
--- index.js
+++ index.js
@@ -1,73 +1,159 @@
+'use strict'
 /*
- Hides away all the junk of creating an Proxy Compy SPDY-ied HTTP server
+ Hides away all the junk of creating an Proxy Protocl Compatible SPDY-ied HTTP server
 
- DOES NOT SUPPORT TLS/SSL stuff yet as we don't need it
+ DOES NOT SUPPORT TLS/SSL stuff yet as we don't need it and it's pretty easy to that without
+ this library (or at least better documented.
+
+ You need to be using at least node4+. might work on node >0.10 to <4 but not tested
 
 */
 
-var http    = require('http');
-var spdy    = require('spdy');
-var proxywrap = require('findhit-proxywrap');
+const http    = require('http');
+const spdy    = require('spdy');
+const proxywrap = require('findhit-proxywrap');
 
 
 module.exports = function createServer(opts, requestListener){
 
-    var server;
-    var proxiedHttp;
+    let server;
 
-    var opts = opts || {};
+    opts = opts || {};
 
     //TODO:
     // make strict mode configurable
     // make spdy opts configurable
     
-    var useProxyProtocol = opts.proxyprotocol === true ? true : false;
-    var isSpdy = opts.spdy === true ? true : false;
+    const useProxyProtocol = opts.proxyprotocol === true ? true : false;
+    const isSpdy = opts.spdy === true ? true : false;
 
-    var spdyOpts = {
+    // TODO: is it even possible to have a non-plain but ssl-less connection?
+    // surely ALPN/NPN only works via TLS?
+    const spdyOpts = {
         plain: true,
         ssl: false
     }
 
-    if(isSpdy) {
-
-        if(useProxyProtocol) {
-
-            // keep a reference just in case
-            spdy.server.HttpsServer = spdy.server.Server;
-
-            // PlainServer exists in spdy 2+
-            if(spdy.server.PlainServer) {
-              /*
-                We do hacky over-writing here because:
-                (this applies for spdy v2+)
-                - proxywrap uses spdy.server.Server as a base.
-                - spdy.Server is actually an https server
-                - spdy.PlainServer is a plain http server
-               */
-              spdy.server.Server = spdy.server.PlainServer;
-            } else {
-              spdy.server.Server = spdy.server.instantiate(http.Server);
-            }
-
-            proxiedHttp = proxywrap.proxy(spdy.server, {strict: false});
-            server = proxiedHttp.createServer(spdyOpts, requestListener)
+    if(isSpdy && useProxyProtocol ) {
+      /*
+        We create this wierd object because:
+        (this applies for spdy v2+)
+        - proxywrap.proxy() uses server.Server as a base, where server is the first argument
+        - spdy.Server is actually an https server
+        - spdy.PlainServer is a plain http server
+       */
+        const base = {
+            Server: spdy.server.PlainServer
+        };
         
-        } else {
-            server = spdy.createServer(spdyOpts, requestListener);
+        // NOTE: this needs some serious work...
+        // we are re-adding info proxywrap parsed from proxyprotocol headers if spdy connection
+        // because spdy hides underlying TCP connection
+        //   req.connection !== req.spdyStream.connection.socket
+        const wrappedRequestListener = function(req, res){
+            // NOTE: we overwrite remote address which means we lose the IP:PORT which
+            // the loadbalancer initiated the connection from
+            // ProxyAddress/Port refer to interface the loadbalancer accepted the 
+            // connection on
+            
+            // If we recieve non-spdy/http2 traffic whilst in spdy/http2 mode it will actually
+            // be a normal stream
+            if(req.isSpdy){
+                // NOTE: values from remoteAddress/Port maybe be affected by proxywraps 'remoteOverride'
+                // option
+                defineRemoteAddress(req.connection, req.spdyStream.connection.socket);
+                defineRemotePort(req.connection, req.spdyStream.connection.socket);
+                defineClientAddress(req.connection, req.spdyStream.connection.socket);
+                defineClientPort(req.connection, req.spdyStream.connection.socket);
+                defineProxyAddress(req.connection, req.spdyStream.connection.socket);
+                defineProxyPort(req.connection, req.spdyStream.connection.socket);
+            }
+            requestListener(req, res)
         }
 
-    } else {
+        const proxiedHttp = proxywrap.proxy(base, {strict: false});
+        server = proxiedHttp.createServer({spdy: spdyOpts}, wrappedRequestListener)
+    }
+    
+    if(isSpdy && !useProxyProtocol) {
+        server = spdy.createServer({spdy: spdyOpts}, requestListener);
+    }
 
-        if(useProxyProtocol) {
-            proxiedHttp = proxywrap.proxy(http, {strict: false});
-            server = proxiedHttp.createServer(requestListener)
-        } else {
-            server = http.createServer(requestListener);
-        }
+    if(!isSpdy && useProxyProtocol) {
+        const proxiedHttp = proxywrap.proxy(http, {strict: false});
+        server = proxiedHttp.createServer(requestListener)
+    }
 
+    if(!isSpdy && !useProxyProtocol){
+        server = http.createServer(requestListener);
     }
 
     return server;
 
+}
+
+/**
+ * Taken from proxywrap as we need to ce-copy/define these on the request handler/listener
+ * or each request as SPDY doesn't use/care/understand abput proxrwrap underneath and req.connection
+ * doesn't directly map to the underlying tcp connection
+ */
+
+function defineRemoteAddress(socket, realSocket){
+  Object.defineProperty(socket, 'remoteAddress', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+      return realSocket.remoteAddress;
+    }
+  });
+}
+
+function defineRemotePort(socket, realSocket){
+  Object.defineProperty(socket, 'remotePort', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+      return realSocket.remotePort
+    }
+  });
+}
+
+function defineClientAddress(socket, realSocket){
+  Object.defineProperty(socket, 'clientAddress', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+      return realSocket.clientAddress
+    }
+  });
+}
+
+function defineClientPort(socket, realSocket){
+  Object.defineProperty(socket, 'clientPort', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+      return realSocket.clientPort
+    }
+  });
+}
+
+function defineProxyAddress(socket, realSocket){
+  Object.defineProperty(socket, 'proxyAddress', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+      return realSocket.proxyAddress
+    }
+  });
+}
+
+function defineProxyPort(socket, realSocket){
+  Object.defineProperty(socket, 'proxyPort', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+      return realSocket.proxyPort
+    }
+  });
 }
